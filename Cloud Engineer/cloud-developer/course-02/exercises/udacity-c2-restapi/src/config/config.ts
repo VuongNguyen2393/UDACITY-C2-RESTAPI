1553863f6c6180bc5598d6e1f3894905f3368622
VuongND9 <nguyendinhvuong.act@gmail.com>
2022-11-10 12:34:02 +0700

    initial project

---
+++ Cloud Engineer/cloud-developer/course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "vuongnd9",
+    "password": "vuongnd9",
+    "database": "vuongnd9",
+    "host": "vuongnd9.ca8mvlhk8seb.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
